c5806d6ee2066c71f85b37d9c47c0e63a2259f36
MaximilianHu <vgshxyzbew511@hotmail.com>
2025-10-18 15:04:16 +0800

    fix

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -10,8 +10,6 @@ import * as dotenv from "dotenv";
 dotenv.config();
 import "solidity-coverage";
 
-import "./tasks/accounts";
-import "./tasks/FHECounter";
 import "./tasks/EncryptedIdentity";
 
 const INFURA_API_KEY: string = process.env.INFURA_API_KEY || "";
